fc214bfb39ae8ada5d35c89d20b60da0d7fce43d
Quinton Smith <smith.quinton42@gmail.com>
2020-07-27 17:24:24 -0400

    added jquery and pokeapi

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log ("Hello World");
+var queryURL = "https://pokeapi.co/api/v2/pokemon/";
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function(response) {
+    console.log(response);
+});
